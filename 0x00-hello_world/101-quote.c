f38220331c0c4cd0548d749c5d5da7202e2acd10
mOhmedelsaYd <mohamedelsaid3963@gmail.com>
2023-02-16 15:07:49 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, s, strlen(s));
+return (1);
+}
