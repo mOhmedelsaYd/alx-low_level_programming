75e806346c726a2fa4fad0eec7722d10ea9c7639
mOhmedelsaYd <mohamedelsaid3963@gmail.com>
2023-02-17 13:10:57 -0800

    hello mo

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+* main - entry point
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+putchar(a + '0');
+putchar('\n');
+return (0);
+}
