1f74175cf985c9f301169e08880d240add0b6ac4
mOhmedelsaYd <mohamedelsaid3963@gmail.com>
2023-02-23 07:18:26 -0800

    8-square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,6 +7,12 @@ void print_square(int size)
 {
 	int i, j;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
@@ -15,5 +21,5 @@ void print_square(int size)
 		}
 		_putchar('\n');
 	}
-
+	}
 }
