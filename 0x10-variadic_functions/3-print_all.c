b2cce491238f2202fe252295f7d596264ecbcecb
mOhmedelsaYd <mohamedelsaid3963@gmail.com>
2023-03-23 08:20:03 -0700

    3-print_all.cclear

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include "variadic_functions.h"
+/**
+ * print_all - prints any combination of ints, floats, chars, and strings
+ *
+ * @format: a string containing the format specifiers for each argument
+ *          'c' for char, 'i' for int, 'f' for float, 's' for char *
+ *
+ * @...: the list of arguments to print
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	char *s;
+
+	va_start(args, format);
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(args, char);
+			break;
+		case 'i':
+			printf("%i", va_arg(args, int);
+			break;
+		case 'f':
+			printf("%f", va_arg(args, double);
+			break;
+		case 's':
+			s = va_arg(args, char *);
+			if (s == NULL)
+			{
+			printf("(nil)");
+			break;
+			}
+			printf("%s", s);
+			break;
+		default:
+			i++;
+			continue;
+		}
+		if (format[i + 1])
+		printf(", ");
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+
+}
