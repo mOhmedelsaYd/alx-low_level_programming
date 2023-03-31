b4313855cb92532e5d7f65643de605989be274f9
mOhmedelsaYd <mohamedelsaid3963@gmail.com>
2023-03-31 11:47:19 -0700

    mo

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,13 +1,8 @@
-/*
- * File: 0-print_listint.c
- * Auth: Brennan D Baraban
- */
-
 #include "lists.h"
 #include <stdio.h>
 
 /**
- * print_listint - Prints all the elements of a listint_t list.
+ * print_list - Prints all the elements of a listint_t list.
  * @h: A pointer to the head of the list_t list.
  *
  * Return: The number of nodes in the list_t list.
