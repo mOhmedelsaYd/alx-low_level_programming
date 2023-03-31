28ed6b7490cbdc8f1a321496a034ed1d6452f05b
mOhmedelsaYd <mohamedelsaid3963@gmail.com>
2023-03-31 11:58:37 -0700

    mo

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * list_len - length of linked list
+ * @h: head
+ * Return: return count of length
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *ptr = h;
+
+	if (ptr == NULL)
+		return (count);
+	while (ptr != NULL)
+	{
+		ptr = ptr->next;
+		count++;
+	}
+	return (count);
+}
