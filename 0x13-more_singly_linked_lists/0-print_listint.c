eadd2e1025b55bc8d08ca60ca039a0d61a679cb8
mOhmedelsaYd <mohamedelsaid3963@gmail.com>
2023-05-02 01:24:18 -0700

    mo

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,3 @@
-
-Here's an implementation of the print_listint function in C that prints all the elements of a linked list:
-
-c
-Copy code
 #include "lists.h"
 
 /**
@@ -12,14 +7,14 @@ Copy code
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t count = 0;
+size_t count = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        count++;
-        h = h->next;
-    }
+while (h != NULL)
+{
+printf("%d\n", h->n);
+count++;
+h = h->next;
+}
 
-    return (count);
+return (count);
 }
