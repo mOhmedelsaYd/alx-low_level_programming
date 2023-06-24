d87747f39ce292d046eecd1c899bad214ae92d28
mOhmedelsaYd <mohamedelsaid3963@gmail.com>
2023-06-24 16:20:25 -0700

    mo

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-gcc -fPIC -c *.c
-gcc -shared -o liball.so *.o
+
+# Get the list of .c files in the current directory
+c_files=$(ls *.c 2>/dev/null)
+
+# Check if there are any .c files present
+if [ -z "$c_files" ]; then
+  echo "No .c files found in the current directory."
+  exit 1
+fi
+
+# Compile and create the dynamic library
+gcc -shared -o liball.so $c_files
+
+# Verify if the compilation was successful
+if [ $? -eq 0 ]; then
+    echo "Dynamic library liball.so created successfully!"
+else
+    echo "Failed to create the dynamic library."
+fi
